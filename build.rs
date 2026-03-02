253799c488b124403e6fd781fa130d6f6e43b373
PatricioRios <patricio_rios2010@live.com>
2026-03-02 18:29:25 -0300

    pre napi

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
+// build.rs
+extern crate napi_build;
+
 fn main() {
-    // En las versiones modernas de UniFFI con macros, no es necesario llamar a generate_scaffolding
-    // si usamos setup_scaffolding!() en lib.rs y anotaciones #[uniffi::export].
-    // El build.rs se deja vacío o para otras tareas.
+    //napi_build::setup();
 }
